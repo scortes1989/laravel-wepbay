24cd7f78dba54e82cd4a1dcf47c97653c57f9b34
Sebastian  Cortes <seba.cortes1989@gmail.com>
2016-09-22 22:32:30 -0300

    commit inicial

---
+++ packages/Scortes1989/Webpay/src/WebPayServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Scortes1989\Webpay;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebPayServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
